a41e14bb1718533aa4d72ec5e83df9aeab3a1994
Jason Vigil <jdvigil@aurorak12.org>
2024-10-01 13:14:36 -0600

    Updated file

--- 5-If, Else and Indentation.py
+++ 5-If, Else and Indentation.py
@@ -25,15 +25,3 @@
 
 # food = ["pizza", "burger", "fries", "salad", "sushi"]
 # print(random.choice(food))
-
-## Project
-
-# Welcome to the first project. We will be building on this project this and next lesson.
-
-# Create a program that plays rock, paper, scissors with the user. Please create a new file, upload it to Google Drive, and share the link with me. 
-
-# Hint: Create a list of the three options and use ramdom.choice() to randomly choose on of the options. Then use if statements to compare the user's choice to the computer's choice and determine the winner.
-# Some things you will need: 
-#   You will need to use the random library
-#   You will need to use the input() function to get the user's choice
-#   You will need to use the print() function to show the user what is going on with your program.
