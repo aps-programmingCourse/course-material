da7b003a643121ab9731bd0aa83ecff50954ba17
jdvigil <36282794+jdvigil@users.noreply.github.com>
2024-10-09 11:43:33 -0600

    Added comments to file.

--- 8-Classes.py
+++ 8-Classes.py
@@ -1,12 +1,59 @@
+## Classes
+# Classes are like a blueprint for constructing objects. Objects
+# are everywhere in python. Objects can have variables called attributes
+# and functions called methods. 
+
+# This is a basic class with some attributes. 
+
 # class Person:
 #     age = 24
 #     name = "John Doe"
 #     location = "Toronto, Ontario"
 
-# john = Person()
-# print(john.age)
-# print(john.name)
-# print(john.location)
+# the variable person becomes an object. We can print out the attributes 
+# as we need them. We can also redefine attributes as well. 
+
+# person = Person()
+# print(person.age)
+# print(person.name)
+# print(person.location)
+
+# person.name = "Jackson Pollock"
+# print(person.name)
+
+###########################################################################
+
+# Let's look at another example. Let's say we're making
+# a game and we have a Character class that we can use
+# as a template. In this case, we have a special
+# function or method that gets run whenever we reference 
+# the class.  
+
+# When using a method inside a class, you need to declare
+# the variables as self.variable. Also, when python creates
+# the object, it passes it as the first argument, which is why 
+# self is the first argument in the __init__ method. Any other
+# arguments can be placed after that.
+
+# You'll notice these attributes are set to whatever the arguments
+# are in the __init__ method
+
+class Character:
+    def __init__(self, h, d, s):
+        self.health = h
+        self.damage = d
+        self.speed = s
+
+warrior = Character(100, 50, 10)
+ninja = Character(80, 40, 40)
+
+# This is printing an "f-string." This is another way of formatting
+# text. With f-strings, you can put expressions directly in the 
+# string. This is the same as printing "Warrior Speed: " + warrior.speed
+
+# print(f"Warrior Speed: {warrior.speed}")
+print(f"Ninja Damage: {ninja.damage}")
+
 
 ####################################################
 
@@ -26,16 +73,3 @@
 # salesperson1 = Salesperson("Nick", "McCullum")
 # salesperson1.makeSale(600)
 # salesperson1.salesReport()
-
-#####################################################
-
-# class Character:
-#     def __init__(self, health, damage, speed):
-#         self.health = health
-#         self.damage = damage
-#         self.speed = speed
-
-# warrior = Character(100, 50, 10)
-# ninja = Character(80, 40, 40)
-
-# print(f"Warrior Speed: {warrior.speed}")
