04ef481a9287ad72f1399ae1759487ba4257d6f8
jdvigil <36282794+jdvigil@users.noreply.github.com>
2024-10-09 15:32:09 -0600

    Added tuples

--- 4-Lists-and-Dictionaries.py
+++ 4-Lists-and-Dictionaries.py
@@ -23,6 +23,19 @@
 # print(newListOfNumbers)
 # print(listOfNumbers[3])
 
+## Tuples
+# Tuples are very similar to lists, but they are immutable,
+# so you can't change them after they're made.
+
+myTuple = ("Spiderman", 18, "Spidey Sense")
+anotherTuple = "Iron Man", 39, "Iron Man Suit"
+
+# Notice that the first example uses parentheses and the
+# second one doesn't, but they're both considered tuples.
+
+# Tuples are faster to calculate than lists, so might be
+# a better choice in some scenarios.
+
 ## Dictionaries
 # Dictionaries contain data that is stored in key-value pairs.
 
