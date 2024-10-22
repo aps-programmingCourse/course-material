ed6deca49769e417dc2f5d9d34d237169d6ff01a
jdvigil <36282794+jdvigil@users.noreply.github.com>
2024-10-22 13:05:43 -0600

    Added comment and one extra line

--- projects/Classes assignment.py
+++ projects/Classes assignment.py
@@ -1,4 +1,4 @@
-## Create Class
+## Write code that will print out the locker number for john
 
 class Student:
     def __init__(self, gradePercent, absences, locker, graduationYear):
@@ -8,3 +8,4 @@ class Student:
         self.graduationYear = graduationYear
 
 john = Student(80, 2, 304, 2025)
+eric = Student(85, 0, 293, 2025)
