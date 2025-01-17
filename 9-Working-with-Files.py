0dda3cf737ea8a0dcb692bf99d1fb82e75334675
jdvigil <36282794+jdvigil@users.noreply.github.com>
2025-01-17 07:25:15 -0700

    Rearranged Comment

--- 9-Working-with-Files.py
+++ 9-Working-with-Files.py
@@ -4,15 +4,17 @@
 # The first way is to use the open() function.
 # The open() function takes two arguments:
 # The first argument is the name of the file to open.
-# The second argument is the mode in which the file should be opened. In this example, we are opening the file for reading, so we use the 'r' mode.
+# The second argument is the mode in which the file should be opened. 
+# In this example, we are opening the file for reading, so we use the 
+# 'r' mode.
 # openGroceriesFile = open("groceries", "r")
 
 # All we've done is open the file. We haven't read anything from it yet. To do that, we need to use the read() method.
 # The read() method returns the contents of the file as a string.
 # readGroceriesText = openGroceriesFile.read()
 
-# Next, in this particular case, it would be helpful to break this string up into a list.
-# We can do that by using the split() method.
+# Next, in this particular case, it would be helpful to break this 
+# string up into a list. We can do that by using the split() method.
 # groceryList = readGroceriesText.split()
 
 # print(groceryList)
@@ -22,8 +24,10 @@
 # openGroceriesFile.close()
 
 # Another way we could open a file is to use the with statement. 
-# The with statement is a way to open a file and automatically close it when we're done with it.
-# It is generally a good idea to use the with statement when working with files because it ensures that the file is closed even if an exception occurs
+# The with statement is a way to open a file and automatically close 
+# it when we're done with it. It is generally a good idea to use the 
+# with statement when working with files because it ensures that the 
+# file is closed even if an exception occurs
 # The with statement sort of works like an if statement.
 # We are using "a+" to open the file for appending and reading.
 # This is because we want to add to the file, not overwrite it.
